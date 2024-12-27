75e09590c2384e13a16f56f8a8fc14701fe34b17
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-12-28 00:49:01 +0100

    Status label adjustment

--- src/main/java/org/sgambe/wtplotter/WTPlotterController.java
+++ src/main/java/org/sgambe/wtplotter/WTPlotterController.java
@@ -5,9 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -25,10 +23,22 @@ public class WTPlotterController {
     private MenuItem preferencesMenuItem;
 
     @FXML
+    private Button plotterButton;
+
+    @FXML
+    private Button replaysButton;
+
+    @FXML
     private TreeView<String> replayListTree;
 
     @FXML
     private AnchorPane rightPane;
+
+    @FXML
+    private Label bottomStatusLabel;
+
+    private ReplayDetailsController replayDetailsController;
+    private PlotterController plotterController;
 
     private Map<TreeItem<String>, File> treeItemToFileMap = new HashMap<>();
 
@@ -83,17 +93,6 @@ public class WTPlotterController {
         replayListTree.setRoot(root);
     }
 
-    public void initialize() {
-        populateTree();
-        preferencesMenuItem.setOnAction(this::openSecondScene);
-        replayListTree.setOnMouseClicked(this::handleTreeViewClick);
-    }
-
-    /**
-     * Handles mouse click events on the TreeView.
-     *
-     * @param event MouseEvent passed from the TreeView
-     */
     private void handleTreeViewClick(MouseEvent event) {
         TreeItem<String> selectedItem = replayListTree.getSelectionModel().getSelectedItem();
 
@@ -103,11 +102,6 @@ public class WTPlotterController {
         }
     }
 
-    /**
-     * Loads the ReplayDetailsView.fxml into the rightPane and populates it with file details.
-     *
-     * @param file The selected replay file.
-     */
     private void loadDetailsView(File file) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("ReplayDetailsView.fxml"));
@@ -115,6 +109,9 @@ public class WTPlotterController {
 
             ReplayDetailsController detailsController = loader.getController();
             detailsController.setFileDetails(file);
+
+            replayDetailsController = loader.getController();
+
 
             rightPane.getChildren().clear();
             rightPane.getChildren().add(detailsView);
@@ -128,6 +125,69 @@ public class WTPlotterController {
             e.printStackTrace();
             System.err.println("Failed to load ReplayDetailsView.fxml");
         }
+    }
+
+    public void loadPlotterView(){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("PlotterView.fxml"));
+            Parent plotterView = loader.load();
+
+            plotterController = loader.getController();
+            plotterController.setBottomStatusLabel(bottomStatusLabel);
+
+            rightPane.getChildren().clear();
+            rightPane.getChildren().add(plotterView);
+
+            AnchorPane.setTopAnchor(plotterView, 0.0);
+            AnchorPane.setBottomAnchor(plotterView, 0.0);
+            AnchorPane.setLeftAnchor(plotterView, 0.0);
+            AnchorPane.setRightAnchor(plotterView, 0.0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Failed to load ReplayDetailsView.fxml");
+        }
+    }
+
+    public void initialize() {
+        populateTree();
+        preferencesMenuItem.setOnAction(this::openSecondScene);
+        replayListTree.setOnMouseClicked(this::handleTreeViewClick);
+        replayListTree.setDisable(true);
+        loadPlotterView();
+
+        replaysButton.setOnAction(event -> {
+            plotterButton.setDisable(false);
+            replaysButton.setDisable(true);
+            replayListTree.setDisable(false);
+            plotterController.shutdown();
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("ReplayDetailsView.fxml"));
+                Parent replayView = loader.load();
+                //WILL LOAD LATEST REPLAY WHEN SQLITE IS IMPLEMENTED
+                rightPane.getChildren().clear();
+                rightPane.getChildren().add(replayView);
+
+
+
+                AnchorPane.setTopAnchor(replayView, 0.0);
+                AnchorPane.setBottomAnchor(replayView, 0.0);
+                AnchorPane.setLeftAnchor(replayView, 0.0);
+                AnchorPane.setRightAnchor(replayView, 0.0);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Failed to load ReplayView.fxml");
+            }
+        });
+
+        plotterButton.setOnAction(event -> {
+            plotterButton.setDisable(true);
+            replaysButton.setDisable(false);
+            replayListTree.setDisable(true);
+            loadPlotterView();
+        });
     }
 
 }

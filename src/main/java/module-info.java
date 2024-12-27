f8ed48714106848710e90e25eeee3edd45e326e7
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2024-12-28 00:48:03 +0100

    Added new modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module org.sgambe.wtplotter {
     requires javafx.fxml;
     requires com.google.gson;
-    requires java.prefs;
     requires javafx.controls;
+    requires java.prefs;
+    requires javafx.swing;
+    requires java.net.http;
 
     opens org.sgambe.wtplotter to javafx.fxml;
     opens org.sgambe.wtplotter.replaydata to javafx.base;

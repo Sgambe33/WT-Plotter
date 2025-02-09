154aa4574dd47e0e74302086e0335640fcafdfc3
Sgambe33 <80386324+Sgambe33@users.noreply.github.com>
2025-02-09 15:34:36 +0100

    Translation files setup

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char* argv[])
 {
 	QApplication a(argc, argv);
+	a.setStyle("Fusion");
+	QTranslator myappTranslator;
+	if (myappTranslator.load(QLocale::system(), "app", "_", ":/i18n"))
+        a.installTranslator(&myappTranslator);
+
 	MainWindow w;
 	w.show();
 	return a.exec();
